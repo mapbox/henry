59c6f74cf3187ef773dddaa33eed4954c3ee9006
Ian Ward <ianshward@gmail.com>
2013-10-21 15:49:03 -0400

    update request, nock. fix all the tests

--- test/henry.test.js
+++ test/henry.test.js
@@ -33,93 +33,76 @@ var v3 = {
   "Expiration" : "ZZZ"
 };
 
+var henry = new Henry();
+
 describe('henry wrapper', function() {
-    before(function() {
-        // Mock client of AWS metadata API
-        this.nock = nock('http://169.254.169.254')
-            .get('/latest/meta-data/iam/security-credentials/')
-            .reply(200, 'role1')
-            .get('/latest/meta-data/iam/security-credentials/role1')
-            .reply(200, v1);
-        this.henry = new Henry();
-    });
     var client = {};
-    var client2 = {
-        key: 'foo'
-    };
-    var client3 = {
-        awsKey: 'foo'
-    };
-    var client4 = {
-        awsKey: null,
-        awsSecret: null
-    };
     it('should fetch credentials when instantiated', function(done) {
-        this.nock
-            .get('/latest/meta-data/iam/security-credentials/')
-            .reply(200, 'role1')
-            .get('/latest/meta-data/iam/security-credentials/role1')
-            .reply(200, v1);
-        this.henry.add(client, function(err) {
+        var metadata = nock('http://169.254.169.254')
+          .get('/2012-01-12/meta-data/iam/security-credentials/')
+          .reply(200, 'role1')
+          .get('/2012-01-12/meta-data/iam/security-credentials/role1')
+          .reply(200, v1);
+        henry.add(client, function(err) {
             assert.equal(client.key, 'XXX');
             assert.equal(client.secret, 'XXX');
             assert.equal(client.token, 'XXX');
             done(err);
         });
-    })
+    });
     it('should refresh the client with new credentials', function(done) {
-        this.nock
-            .get('/latest/meta-data/iam/security-credentials/')
-            .reply(200, 'role1')
-            .get('/latest/meta-data/iam/security-credentials/role1')
-            .reply(200, v2)
-        this.henry.refresh(function(err, credentials) {
+        var metadata = nock('http://169.254.169.254')
+          .get('/2012-01-12/meta-data/iam/security-credentials/')
+          .reply(200, 'role1')
+          .get('/2012-01-12/meta-data/iam/security-credentials/role1')
+          .reply(200, v2)
+        henry.refresh(function(err, credentials) {
             assert.equal(client.key, 'YYY');
             assert.equal(client.secret, 'YYY');
             assert.equal(client.token, 'YYY');
             done(err);
         });
-    })
+    });
     it('should use same credentials on non-200 from metadata API outer call', function(done) {
-        this.nock
-            .get('/latest/meta-data/iam/security-credentials/')
-            .reply(500, 'role1')
-        this.henry.refresh(function(err, credentials) {
+        var metadata = nock('http://169.254.169.254')
+          .get('/2012-01-12/meta-data/iam/security-credentials/')
+          .reply(500, 'role1');
+        henry.refresh(function(err, credentials) {
             if (err.code != 500) return done(err);
             assert.equal(client.key, 'YYY');
             assert.equal(client.secret, 'YYY');
             assert.equal(client.token, 'YYY');
             done();
         });
-    })
+    });
     it('should use same credentials on non-200 from metadata API inner call', function(done) {
-        this.nock
-            .get('/latest/meta-data/iam/security-credentials/')
-            .reply(200, 'role1')
-            .get('/latest/meta-data/iam/security-credentials/role1')
-            .reply(500, '')
-        this.henry.refresh(function(err, credentials) {
+        var metadata = nock('http://169.254.169.254')
+          .get('/2012-01-12/meta-data/iam/security-credentials/')
+          .reply(200, 'role1')
+          .get('/2012-01-12/meta-data/iam/security-credentials/role1')
+          .reply(500, '')
+        henry.refresh(function(err, credentials) {
             if (err.code != 500) return done(err);
             assert.equal(client.key, 'YYY');
             assert.equal(client.secret, 'YYY');
             assert.equal(client.token, 'YYY');
             done();
         });
-    })
+    });
     it('should update credentials on setInterval', function(done) {
-        this.nock
-            .get('/latest/meta-data/iam/security-credentials/')
-            .reply(200, 'role1')
-            .get('/latest/meta-data/iam/security-credentials/role1')
-            .reply(200, v1)
-            .get('/latest/meta-data/iam/security-credentials/')
-            .reply(200, 'role1')
-            .get('/latest/meta-data/iam/security-credentials/role1')
-            .reply(200, v1)
-            .get('/latest/meta-data/iam/security-credentials/')
-            .reply(200, 'role1')
-            .get('/latest/meta-data/iam/security-credentials/role1')
-            .reply(200, v3)
+        var metadata = nock('http://169.254.169.254')
+          .get('/2012-01-12/meta-data/iam/security-credentials/')
+          .reply(200, 'role1')
+          .get('/2012-01-12/meta-data/iam/security-credentials/role1')
+          .reply(200, v1)
+          .get('/2012-01-12/meta-data/iam/security-credentials/')
+          .reply(200, 'role1')
+          .get('/2012-01-12/meta-data/iam/security-credentials/role1')
+          .reply(200, v1)
+          .get('/2012-01-12/meta-data/iam/security-credentials/')
+          .reply(200, 'role1')
+          .get('/2012-01-12/meta-data/iam/security-credentials/role1')
+          .reply(200, v3);
         var henry = new Henry({ interval: 500 });
         henry.add(client, function(err, credentials) {
             assert.equal(client.key, 'XXX');
@@ -132,14 +115,14 @@ describe('henry wrapper', function() {
                 done();
             }, 1000);
         });
-    })
+    });
     it('should update the client with new credentials, using custom props', function(done) {
-        this.nock
-            .get('/latest/meta-data/iam/security-credentials/')
-            .reply(200, 'role1')
-            .get('/latest/meta-data/iam/security-credentials/role1')
-            .reply(200, v1);
-        this.henry.add(client, {
+        var metadata = nock('http://169.254.169.254')
+          .get('/2012-01-12/meta-data/iam/security-credentials/')
+          .reply(200, 'role1')
+          .get('/2012-01-12/meta-data/iam/security-credentials/role1')
+          .reply(200, v1);
+        henry.add(client, {
             key: 'awsKey',
             secret: 'awsSecret',
             token: 'awsToken',
@@ -151,5 +134,5 @@ describe('henry wrapper', function() {
             assert.equal(client.awsExpiration, 'XXX');
             done(err);
         });
-    })
+    });
 });
